b0f59f7245808e37444348dc77a2b145aed2fce9
Jan Klass <kissaki@posteo.de>
2019-07-13 15:24:46 +0200

    Replace Qt program source with .NET source

--- installer/build-installer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-SETLOCAL
-SET QTIBIN=C:\dev\Qt\Tools\QtInstallerFramework\3.0\bin
-
-%QTIBIN%\binarycreator.exe --offline-only -c config\config.xml -p packages Videocutter-Installer.exe
-IF ERRORLEVEL 1 (PAUSE && EXIT 1)
