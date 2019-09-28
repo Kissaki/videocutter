3251e586d8d1d05e90b06b751dcc3b925df06b0e
Jan Klass <kissaki@posteo.de>
2019-09-28 10:07:28 +0200

    Code: Add AssemblyInfo.cs

---
+++ src/Videocutter/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KCode.Videocutter.Properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KCode.Videocutter")]
+[assembly: AssemblyCopyright("Copyright © Jan Klass 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2087d1e9-70ad-41e8-8125-1c186d64b135")]
+
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
